95bbe5453beb850b40dc0699715926a75b474c66
StevAlberts <ogsteval@gmail.com>
2022-02-17 14:13:48 +0300

    Added voting apis

--- app/src/main/java/com/nextcloud/talk/VoteSheetVote.kt
+++ app/src/main/java/com/nextcloud/talk/VoteSheetVote.kt
@@ -2,11 +2,32 @@ package com.nextcloud.talk
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.RadioButton
 
 class VoteSheetVote : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 //        setContentView(R.layout.vote_sheet_vote)
+
+//        fun onRadioButtonClicked(view: View) {
+//            if (view is RadioButton) {
+//                // Is the button now checked?
+//                val checked = view.isChecked
+//
+//                // Check which radio button was clicked
+//                when (view.getId()) {
+//                    R.id.radio_pirates ->
+//                        if (checked) {
+//                            // Pirates are the best
+//                        }
+//                    R.id.radio_ninjas ->
+//                        if (checked) {
+//                            // Ninjas rule
+//                        }
+//                }
+//            }
+//        }
 
     }
 }
