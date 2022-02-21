f96e5aa020125b4ddc95aac4c63718c8981bf399
OfficialJamesUsiu <jaycomputertechnologies@gmail.com>
2022-02-21 09:52:33 +0300

    screen share update/ modification

--- app/src/main/java/com/nextcloud/talk/activities/CallActivity.java
+++ app/src/main/java/com/nextcloud/talk/activities/CallActivity.java
@@ -673,16 +673,16 @@ public class CallActivity extends CallBaseActivity {
         int columns;
         int participantsInGrid = participantDisplayItems.size();
         if (getResources() != null && getResources().getConfiguration().orientation == Configuration.ORIENTATION_PORTRAIT) {
-            if (participantsInGrid > 2) {
-                columns = 2;
+            if (participantsInGrid > 1) {
+                columns = 1;
             } else {
                 columns = 1;
             }
         } else {
             if (participantsInGrid > 2) {
-                columns = 3;
+                columns = 1;
             } else if (participantsInGrid > 1) {
-                columns = 2;
+                columns = 1;
             } else {
                 columns = 1;
             }
